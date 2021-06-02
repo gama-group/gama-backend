5311ee9de8236dee87f3de2a7bf9221017485b71
gustavo4passos <gustavo4passos@gmail.com>
2021-06-02 01:39:08 -0300

    Prepares config for heroku

--- ormconfig.js
+++ ormconfig.js
@@ -2,10 +2,16 @@ module.exports = {
   type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   logging: false,
+  ssl: process.env.SSL_ENABLED || true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
   entities: [
     process.env.TYPEORM_ENTITIES
   ],
