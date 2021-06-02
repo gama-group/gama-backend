5ed10ba5457a4b53a54e21721cce8f8bfdd3df39
gustavo4passos <gustavo4passos@gmail.com>
2021-06-02 01:18:50 -0300

    Adds typeorm variables to .env

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  type: 'sqlite',
-  host: 'localhost',
-  port: 3333,
-  username: 'test',
-  password: 'test',
-  database: 'db',
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   logging: false,
   entities: [
     process.env.TYPEORM_ENTITIES
